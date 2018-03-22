19c51affe1723612b8b1efa3af77e916d35fd89b
Pinae <jonny@pinae.net>
2018-03-23 00:16:17 +0100

    Sketched a model structure

--- Model.py
+++ Model.py
@@ -1,25 +1,30 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 from keras.models import Model
-from keras.layers import Input, Conv2D
+from keras.layers import Input, Conv2D, TimeDistributed, Permute, Reshape, Dense
 from keras.layers.advanced_activations import LeakyReLU
 from keras.initializers import RandomNormal
 
 
-def conv_block_d(input_tensor, f, use_instance_norm=True):
-    x = input_tensor
-    x = Conv2D(f, kernel_size=4, strides=2, kernel_initializer=RandomNormal(0, 0.02), use_bias=False, padding="same")(x)
-    x = LeakyReLU(alpha=0.2)(x)
-    return x
-
-
 inp = Input(batch_shape=(None,), shape=(1280, 720, 3))
-x = Conv2D(64, kernel_size=5, strides=1, kernel_initializer=RandomNormal(0, 0.02), use_bias=True,
-           padding="same", data_format="channels_last")(inp)
+x = TimeDistributed(Conv2D(64, kernel_size=5, strides=1, kernel_initializer=RandomNormal(0, 0.02), use_bias=True,
+                           padding="same", data_format="channels_last")(inp))
+x = TimeDistributed(LeakyReLU(alpha=0.2)(x))
+x = TimeDistributed(Conv2D(128, kernel_size=5, strides=2, kernel_initializer=RandomNormal(0, 0.02), use_bias=False,
+                           padding="same", data_format="channels_last")(x))
+x = TimeDistributed(LeakyReLU(alpha=0.2)(x))
+x = TimeDistributed(Conv2D(256, kernel_size=5, strides=2, kernel_initializer=RandomNormal(0, 0.02), use_bias=False,
+                           padding="same", data_format="channels_last")(x))
+x = TimeDistributed(LeakyReLU(alpha=0.2)(x))
+x = Reshape((1280, 720, 3*256))(Permute((2, 3, 1, 4))(x))
+x = Conv2D(256, kernel_size=3, strides=1, kernel_initializer=RandomNormal(0, 0.02), use_bias=True,
+           padding="same", data_format="channels_last")(x)
 x = LeakyReLU(alpha=0.2)(x)
-#x = GaussianNoise(0.05)(inp)
-x = conv_block_d(inp, 64, False)
-x = conv_block_d(x, 128, False)
-x = conv_block_d(x, 256, False)
-out = Conv2D(1, kernel_size=4, kernel_initializer=RandomNormal(0, 0.02), use_bias=False, padding="same", activation="sigmoid")(x)
+x = Conv2D(256, kernel_size=4, strides=2, kernel_initializer=RandomNormal(0, 0.02), use_bias=False,
+           padding="same", data_format="channels_last")(x)
+x = LeakyReLU(alpha=0.2)(x)
+x = Conv2D(256, kernel_size=4, strides=2, kernel_initializer=RandomNormal(0, 0.02), use_bias=False,
+           padding="same", data_format="channels_last")(x)
+x = LeakyReLU(alpha=0.2)(x)
+out = Dense(2, activation="softmax", use_bias=True)(x)
 model = Model(inputs=[inp], outputs=out)
